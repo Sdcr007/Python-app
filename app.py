61a8561c191107a532fc5a0fd3a7d72c0b476dff
Shivam Singh (External - Magnit Global Sweden II AB) <shivam.singh2@ingka.com>
2024-03-06 13:03:47 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -7,5 +8,4 @@ def hello():
     return 'Hello, World! This is my Python application deployed on Google Cloud!'
 
 if __name__ == '__main__':
-    from waitress import serve
-    app.run(host='0.0.0.0', port=8080)
+    serve(app, host='0.0.0.0', port=8080)
